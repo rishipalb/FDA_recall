adb69ba41e173a286f5e782fa1b4e0cc3bb08da1
rishipalb <rishipal.b@gmail.com>
2022-12-09 14:46:23 -0500

    Add files via upload

--- pages/FDA_Recall_Status.py
+++ pages/FDA_Recall_Status.py
@@ -8,8 +8,9 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
 from dateutil.relativedelta import relativedelta # to add days or years
 
+
 st.markdown("# FDA Product Recall 🎈")
-st.markdown("FDA recalls in this app that is available @ https://www.fda.gov/media/145551/download. Source: https://www.fda.gov/about-fda/open-government-fda-data-sets/recalls-data-sets. Note: The recall list is only Firm-issued recall dataset.")
+st.markdown("This app is designed to provide a easy-to-use user interface for checking the recent recalls enforced by the Food and Drug Administration (FDA). It also provides a visual analysis of the FDA’s recall history, presents trends and shows the most common recall reasons.")
 
 st.sidebar.markdown("# Recent Recall Status 🎈")
 
@@ -43,15 +44,16 @@ df = pd.read_excel('data/recalls.xlsx')
 #df["Date"] = pd.to_datetime(df["Date"])
 #df["Date"] = df["Date"].dt.strftime("%Y-%m-%d") #MMM DD, YYYY
 df["Date"] = df["Date"].astype('datetime64[ns]')
-df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d") # MMM DD, YYYY
+df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d") # MMM DD, YYYY, "%Y-%m-%d"
 #st.write(df.dtypes)
 #st.write(df)
+
 
 test_sort = df.sort_values(['Date'], ascending=[False])[:20]
 #test_sort = pd.DataFrame('test_sort')
 #st.write(test_sort)
 
-slider = st.sidebar.slider('Select the range in days', min_value=0, value=30, max_value=60)
+slider = st.sidebar.slider('**Select the range in days**', min_value=0, value=30, max_value=60)
 selected_date=end_date -relativedelta(days=slider)
 
 
@@ -62,10 +64,15 @@ st.sidebar.table(pd.DataFrame([[selected_date, end_date]],
                       index=['date']))
 
 st.sidebar.markdown("# Historical data 🎈")
-min_bar = st.sidebar.slider("Minimum bars for charts", 5, 40, 5, 1)
+min_bar = st.sidebar.slider("Minimum bars for charts", 5, 40, 10, 1)
 image_size = st.sidebar.slider("Word Cloud Image Width", 100, 800, 400, 10)
-st.sidebar.markdown("Exclude terminated recalls?")
+
+st.sidebar.markdown("**Exclude terminated recalls?**")
+st.sidebar.write("Select this option to remove recalls that were resolved or are no longer active.")
 rem_terminated = st.sidebar.checkbox('Yes')
+
+cat_list = ['Animal & Veterinary', 'Cosmetics', 'Dietary Supplements', 'Drugs', 'Food & Beverages', 'Medical Devices', 'Tobacco', 'Other']
+cat_list.sort()
 
 # Select DataFrame rows between two dates using DataFrame.isin()
 #df2 = df[df["Date"].isin(pd.date_range(selected_date, end_date))]
@@ -87,6 +94,8 @@ if rem_terminated:
 
 # Constrict dataframe between two dates    
 df2 = df.query('Date > @selected_date and Date < @end_date')
+
+#st.write(cat_list)
 
 # Join string from each row of a column
 text = " ".join(review for review in df['Product-Types'].astype(str))
@@ -121,8 +130,13 @@ with tab2:
         y=alt.Y("Brand-Names:O", sort='-x')
         )
         st.altair_chart(brand)
-        st.write(brand_count.iloc[0:1,0:1])
-    st.write('* Results shown above are for historical data.')
+        #st.write(brand_count.iloc[0:1,0:1])
+        
+    if rem_terminated:
+        st.write(f"If terminated recalls were excluded, the product category with highest recalls was: {type_count['Product-Types'].values[0]}. The brand with highest recall was: {brand_count['Brand-Names'].values[0]}.")
+    else:
+        st.write(f"Overall, the product category with highest recalls was: {type_count['Product-Types'].values[0]}. The brand with most recalls was: {brand_count['Brand-Names'].values[0]}.")
+
 with tab3:
     st.markdown("### Word cloud of Product-Types")
     text = " ".join(review for review in df['Product-Types'].astype(str))
@@ -132,10 +146,9 @@ with tab3:
     cloud = WordCloud(stopwords=stopwords, background_color="white", width=800, height=400)
     wc = cloud.generate(text)
     word_cloud = cloud.to_file('wordcloud.png')
-    st.image(wc.to_array(), width=image_size)
+    st.image(wc.to_array(), width=image_size) # wc.to_array()
     st.write("There are {} words in the combination of all cells in column Product-Types.".format(len(text)))
-    
-    
+
     st.markdown("### Word cloud of Recall-Reason-Description")
     text1 = " ".join(review for review in df['Recall-Reason-Description'].astype(str))
     cloud1 = WordCloud(stopwords=stopwords, background_color="yellow", width=800, height=400)
@@ -144,9 +157,44 @@ with tab3:
     st.image(word_cloud1.to_array(), width=image_size)
     st.write("There are {} words in the combination of all cells in column Recall-Reason-Description.".format(len(text1)))
     #st.write(wc1.words_.keys())
-    st.write('* Results shown above are for historical data.')
+    
+with st.container():
+    st.markdown("### Recalls each month per product type 🎉")
+    text_1 = st.selectbox('**Select a category:**', cat_list)
 
-st.write("**Disclaimer:** This is app is for experimental and educational purpose only. The dataset may not be current and therefore would result in inaccurate and outdate information. The developer does not take any responsibility in the event of potential harm caused by the inadvertent use of this app.")
+    #st.info("**Example:** whole foods")
+    if text_1 != '':
+        new_df1 = df[(df['Product-Types'].str.contains(text_1, case=False, na=False))].sort_values(by=['Date'], ascending=False)
+        new_df1 = new_df1.sort_values(['Date'], ascending=[False])
+        #st.markdown("# **Results of custom search** 🎉")
+        #st.write('**Text input results:**', new_df1)
+        if not new_df1.empty:
+            # write dataframe to screen
+            new_df1["Date"] = new_df1['Date'].dt.strftime('%Y/%m')
+            new_df1['Terminated Recall'] = new_df1['Terminated Recall'].fillna("Not Terminated")
+            prod_count = pd.DataFrame()
+
+            prod_count = new_df1[['Date', 'Product-Types', 'Recall-Reason-Description', 'Terminated Recall']]
+            #terminate_count['Date'] = df['Date'].dt.month
+            #prod_count=pd.DataFrame(prod_count)
+            prod_count = prod_count.groupby(['Date', 'Terminated Recall'])['Date'].count().reset_index(name='count').sort_values('Date')
+            #st.write(prod_count.dtypes)
+            prod_count["Date"] = prod_count["Date"].astype('datetime64[ns]')
+
+            st.markdown("**Line chart for number of recalls each month per product type**")
+            line_all = alt.Chart(prod_count).mark_line().encode(
+                x='Date', y='count:Q', color='Terminated Recall').configure_axis(
+                    grid=False
+                    ).interactive()
+
+            st.altair_chart(line_all, use_container_width=800)
+
+        else:
+            st.write("**Recall history:** No results.")
+
+st.info('**Live update:** FDA recalls in this app is available @ https://www.fda.gov/media/145551/download. **Source:** https://www.fda.gov/about-fda/open-government-fda-data-sets/recalls-data-sets. **Note:** The recall list is only Firm-issued recall dataset.')
+st.warning('* Results shown above are for historical data.')
+st.info("**Disclaimer:** This is app is for experimental and educational purpose only. The dataset may not be current and therefore would result in inaccurate and outdate information. The developer does not take any responsibility in the event of potential harm caused by the inadvertent use of this app.")
 
     
 
