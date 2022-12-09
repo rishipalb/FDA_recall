d7b733545b5a829c6bd63d8605e5d88285233b8a
rishipalb <rishipal.b@gmail.com>
2022-12-09 14:56:21 -0500

    Add files via upload

--- Search_Options.py
+++ Search_Options.py
@@ -52,7 +52,7 @@ else:
 #Check to verify the terminated sidebar
 #st.write(df2)
 
-tab1, tab2, tab3 = st.tabs(['Keyword Search', 'Multi Search', 'Nested'])
+tab1, tab2 = st.tabs(['Keyword Search', 'Multi Search'])
 
 
 with tab1:
@@ -83,34 +83,6 @@ with tab2:
         st.markdown("Results of custom search 🎉")
         st.table(new_df)
 
-#text_1 = st.sidebar.text_input('Search keywords', 'Dole')
-#new_df1 = df[(df['Product-Types'].isin(text_1)) & (df['Brand-Names'].isin(text_1))]
-#st.write('Text input results:', new_df1)
-with tab3:
-    df4 = df2.sort_values(['Product-Types' , 'Brand-Names'], ascending=[True, True])
-    #df4['Product-Types'] = df4['Product-Types'].astype(str)
-    st.write(df4.dtypes)
-    #with col1:
-    product_list = list(df4['Product-Types'].unique())
-    st.write(product_list)
-    #product_list = product_list.sort()
-    #product_list.sort()df3['Brand-Names']
-    product1 = st.multiselect("**Select product types?**", product_list)
-    brand_list = list(set(df4.loc[(df4.iloc[:, 3].isin(product1))][df4.columns[1]]))
-    st.write(brand_list)
-    brand1 = st.multiselect("**Select brands that are recalled_list?**", brand_list)
-    
-    
-    product = st.multiselect("**Select product types?**", list(set(df4.iloc[:, 3])))
-    
-    #with col2:
-    brand = st.multiselect("**Select brands that are recalled?**", list(set(df4.loc[(df4.iloc[:, 3].isin(product))][df4.columns[1]])))
-        # Filter dataframe
-    new_df = df4[(df4['Product-Types'].isin(product)) & (df4['Brand-Names'].isin(brand))].sort_values(by=['Date'], ascending=False)
-    if not new_df.empty:
-        # write dataframe to screen
-        st.markdown("Results of custom search 🎉")
-        st.table(new_df)
 
 
 st.info("**Disclaimer:** This is app is for experimental and educational purpose only. The dataset may not be current and therefore would result in inaccurate and outdate information. The developer does not take any responsibility in the event of potential harm caused by the inadvertent use of this app.")
